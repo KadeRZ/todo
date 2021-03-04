415bdfbf0131d3b2269df987209ebbe70dbbef4d
Kade Zimmerman <kaderz0021@gmail.com>
2021-03-04 16:54:09 -0700

    getting the buttons in the right place

--- js/main.js
+++ js/main.js
@@ -1,35 +1,35 @@
 const chats = {};
-const tasks = {};
+const message = {}
 const users = [];
 let currentChat;
-let currentTask;
-
 
 document
   .getElementById('new-chat-button')
   .addEventListener('click', addNewChat);
-
+  document
+  .getElementById('new-chat-button1')
+  .addEventListener('click', addNewMessage);
+  document
+  .getElementById('new-chat-input')
+  .addEventListener('keydown', (event) => {
+    if (event.key == 'Enter') {
+      addNewChat();
+    }
+  });
 print();
 
 
 function print() {
   // chats
   let chatsHtml = '';
-  let tasksHtml = '';
 
   Object.values(chats).forEach((chat) => {
-    chatsHtml += `<li class="list-group-item" onclick="selectCurrentChat('${chat.id}')">${chat.name}</li>`;
-  });
-  Object.values(tasks).forEach((task) => {
-    tasksHtml += `<li class="list-group-item" onclick="selectCurrentTask('${task.id}')">${task.name}</li>`;
+    chatsHtml += `<li class="list-group-item" onclick="selectCurrentChat('${chat.id}')">${chat.name}</li> <button class="btn btn-primary">Remove</button></div>`;
   });
   document.getElementById('chats').innerHTML = chatsHtml;
-  document.getElementById('tasks').innerHTML = tasksHtml;
 
   // current chat name
   document.getElementById('current-chat-name').innerText = currentChat.name;
-  document.getElementById('current-task-name').innerText = currentTask.name;
-
   // current chat message window
   let chatWindowHtml = '';
   currentChat.messages.forEach((message) => {
@@ -40,15 +40,6 @@ function print() {
         </div>
     `;
   });
-  let taskWindowHtml = '';
-  currentTask.messages.forEach((message) => {
-    taskWindowHtml += `
-      <div class="message">
-        <p>${message.text}</p>
-        <p>${message.username}</p>
-      </div>
-      `;
-  });
   document.getElementById('chat-window').innerHTML = chatWindowHtml;
   document.getElementById('task-window').innerHTML = taskWindowHtml;
   // users
@@ -58,6 +49,8 @@ function print() {
     <div>
         <label for="${user.username}">${user.username}</label>
         <input id="${user.username}">
+        <button onclick="addNewMessage('${currentChat.id}')" class="btn btn-primary">Submit</button>
+
     </div>`;
   });
   document.getElementById('users').innerHTML = usersHtml;
@@ -76,32 +69,17 @@ function addNewChat() {
     print();
   }
 }
-function addNewTask() {
-  const taskName = document.getElementById('new-task-input').value;
-  if (taskName) {
-    // create the list
-    const newTask = new Task(taskName);
-    // add list to lists
-    tasks[newTask.id] = newTask;
-    // clear out the input box
-    document.getElementById('new-task-input').value = '';
-    //print again
-    print();
-  }
+function addNewMessage(chatId, username) {
+  const text = document.getElementById(username).value;
+  const newMessage = new Message(text, username);
+  chats[chatId].addMessage(newMessage);
+  print();
 }
-// function myFunction() {
-//   let myobj = document.getElementById("new-chat-button");
-//   myobj.remove();
-// }
-
 function selectCurrentChat(chatId) {
   currentChat = chats[chatId];
   // print again
   print();
 }
-function selectCurrentTask(taskId) {
-  currentTask = tasks[taskId];
-  // print again
-  print();
-}
+
+
 
